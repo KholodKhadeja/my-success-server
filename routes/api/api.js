78b7a14f9ba18f3eab1e306cd00c366935667e7b
KholodKhadeja <kholod.khadeja@hotmail.com>
2023-01-30 20:16:49 +0200

    fisrt commit serer

---
+++ routes/api/api.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const router = express.Router();
+
+// const bizCardRouter = require("../api/bizcards")
+// const authRouter = require("../api/auth");
+ 
+
+// router.use("/bizcards", bizCardRouter);
+// router.use("/auth", authRouter);
+
+module.exports=router;
