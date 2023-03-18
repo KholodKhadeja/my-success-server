a3ef53b8c81c9220fb360de3fdf0fd3fbaed3aac
KholodKhadeja <kholod.khadeja@hotmail.com>
2023-03-18 22:27:32 +0200

    updates

--- app.js
+++ app.js
@@ -16,12 +16,13 @@ app.use(logger('dev'));
 app.use(express.json());
 // app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use("/api", apiRouter);
-// app.use("/api/*", (req, res) => {
-//   throw new Error("error thrown navigate to");
-// });
+/* connecting react prpejct*/
+app.use(express.static(path.join(__dirname, "build")));
+app.get("*", (req, res) => {
+  res.sendFile(path.join(__dirname, "build", "index.html"));
+});
 
 
 module.exports = app;
