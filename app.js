c941c748ccbf6e089d4f67bd6072b077a701b245
KholodKhadeja <kholod.khadeja@hotmail.com>
2023-03-20 23:22:44 +0200

    update cors

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 const apiRouter = require("./routes/api/api");
+const cors= require("cors");
 
 
 var app = express();
@@ -18,7 +19,7 @@ var app = express();
 // // view engine setup
 // app.set('views', path.join(__dirname, 'views'));
 // app.set('view engine', 'jade');
-
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 // app.use(express.urlencoded({ extended: false }));
