f94408775b0a76e59e6a0c078f4960d9c984edb3
KholodKhadeja <kholod.khadeja@hotmail.com>
2023-03-29 22:24:52 +0300

    user update

--- models/user.model.js
+++ models/user.model.js
@@ -28,7 +28,7 @@ const selectAllUsers = () =>{
     return User.find({});
 };
 const getUserById = (id)=>{
-    return User.findById(_id);
+    return User.findById(id);
 }
 const getUserByEmail = (email)=>{
     return User.findOne({email}).exec();
