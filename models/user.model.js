373b03a183434f3e524e1437fe364191cb4ad630
KholodKhadeja <kholod.khadeja@hotmail.com>
2023-03-31 21:52:01 +0300

    data type update

--- models/user.model.js
+++ models/user.model.js
@@ -14,7 +14,7 @@ const userSchema = new Schema({
       favlessons: [{
         type: Schema.Types.ObjectId,
         ref: "lessons" }],
-    profileImg: { data: Buffer, type: String},
+    profileImg: { data: Buffer, contentType: String},
     userstatus:{type: Boolean, default: true,required:true}
 })
 
