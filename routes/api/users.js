a4456f4d12a8a145f52d3a84ccf9a27fe7001391
KholodKhadeja <kholod.khadeja@hotmail.com>
2023-04-05 00:41:53 +0300

    fix 6

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const User= require("../../models/user.model");
-const Lesson=require("../../models/lesson.model");
+const {Lesson}=require("../../models/lesson.model");
 
 const { selectAllUsers,
     createNewUser, 
