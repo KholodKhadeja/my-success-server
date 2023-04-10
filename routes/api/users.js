4e56d66df6f92f09f44f232358801887e0464328
KholodKhadeja <kholod.khadeja@hotmail.com>
2023-04-10 17:03:17 +0300

    يلب

--- routes/api/users.js
+++ routes/api/users.js
@@ -2,6 +2,7 @@ const express = require("express");
 const router = express.Router();
 const User= require("../../models/user.model");
 const {Lesson}=require("../../models/lesson.model");
+const ObjectId = mongoose.Types.ObjectId;
 
 const { addStudentToStudentArrayOfaLesson, getLessonById, deleteLessonById } = require("../../models/lesson.model");
 const { selectAllUsers,
