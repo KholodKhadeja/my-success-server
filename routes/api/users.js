f0a93e82c2827a79c0985a8162e4d4fc08d31428
KholodKhadeja <kholod.khadeja@hotmail.com>
2023-04-10 13:27:14 +0300

    post

--- routes/api/users.js
+++ routes/api/users.js
@@ -74,7 +74,7 @@ router.post('/:userId/mylessons', async (req, res) => {
   });
 
   /*assign lesson to student*/
-  router.patch('/:studentId/assignlesson/:lessonId', async (req, res) => {
+  router.post('/:studentId/assignlesson/:lessonId', async (req, res) => {
     const { studentId, lessonId  } = req.params;
     const theUser = await getUserById(studentId);
     try { 
