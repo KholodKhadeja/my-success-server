04f82a3678f1f0f95efb1255d77d273b0c362aa7
KholodKhadeja <kholod.khadeja@hotmail.com>
2023-04-10 17:08:36 +0300

    moongose

--- routes/api/users.js
+++ routes/api/users.js
@@ -1,4 +1,5 @@
 const express = require("express");
+const mongoose = require("mongoose");
 const router = express.Router();
 const User= require("../../models/user.model");
 const {Lesson}=require("../../models/lesson.model");
