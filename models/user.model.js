4d6491f1defe8109582956c98869f16ccc6cad9f
KholodKhadeja <kholod.khadeja@hotmail.com>
2023-04-11 05:30:25 +0300

    fix

--- models/user.model.js
+++ models/user.model.js
@@ -91,6 +91,5 @@ module.exports={User,
     getUserById,
     getUserByEmail,
     updatePasswordById,
-    updateUserSpecificLessonByUserId,
     updateUserFavLessonById,deleteLessonFromMyLesson
     };
