17362f1febc13c1f71c8784c607b00bfa9505481
KholodKhadeja <kholod.khadeja@hotmail.com>
2023-05-05 12:53:36 +0300

    2

--- app.js
+++ app.js
@@ -9,16 +9,6 @@ const cors= require("cors");
 
 var app = express();
 
-// Middleware function to add CORS headers
-// app.use((req, res, next) => {
-//     res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3000');
-//     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-//     res.setHeader('Access-Control-Allow-Headers', 'Content-Type, Authorization');
-//     next();
-//   });
-// // view engine setup
-// app.set('views', path.join(__dirname, 'views'));
-// app.set('view engine', 'jade');
 app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
